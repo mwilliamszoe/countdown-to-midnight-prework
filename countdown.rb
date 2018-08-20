336fa5ab00ad65440d53dfdc93c8700657e2ec84
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-20 19:22:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+counter = n
+while counter >=0
+  puts "#{n}"
+  counter +=1
+end
+  puts "HAPPY NEW YEAR!"
+end
+countdown(4)
+
+def countdown(number)
+  while number > 0
+    puts "#{number} seconds left!"
+    number -= 1
+  end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
